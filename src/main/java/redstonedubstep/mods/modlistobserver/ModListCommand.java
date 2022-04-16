5e77ddc414afe23b09465af6f209857eb12b243d
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-04-16 15:40:21 +0200

    Changed command output

--- src/main/java/redstonedubstep/mods/modlistobserver/ModListCommand.java
+++ src/main/java/redstonedubstep/mods/modlistobserver/ModListCommand.java
@@ -2,6 +2,7 @@ package redstonedubstep.mods.modlistobserver;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.mojang.authlib.GameProfile;
@@ -12,11 +13,13 @@ import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.GameProfileArgument;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.TextComponentUtils;
 import net.minecraft.util.text.TranslationTextComponent;
 
 public class ModListCommand {
 	public static void register(CommandDispatcher<CommandSource> dispatcher) {
-		dispatcher.register(Commands.literal("modlist").requires(player -> player.hasPermission(3)).executes(ctx -> getModList(ctx, ctx.getSource().getPlayerOrException().getGameProfile(), false))
+		dispatcher.register(Commands.literal("modlist").requires(player -> player.hasPermission(3))
+				.executes(ctx -> getModList(ctx, ctx.getSource().getPlayerOrException().getGameProfile(), false))
 				.then(Commands.argument("target", GameProfileArgument.gameProfile())
 						.executes(ctx -> getModList(ctx, GameProfileArgument.getGameProfiles(ctx, "target"), false))
 						.then(Commands.literal("all")
@@ -30,28 +33,31 @@ public class ModListCommand {
 	}
 
 	private static int getModList(CommandContext<CommandSource> ctx, Collection<GameProfile> targets, boolean allMods) {
-		int success = 0;
+		Set<String> modList = new HashSet<>();
+		TranslationTextComponent message;
 
 		for (GameProfile target : targets) {
-			Set<String> modList = allMods ? ModListObserver.getAllMods(target) : ModListObserver.getCurrentMods(target);
-			TranslationTextComponent message;
-
-			if (modList != null) {
-				message = new TranslationTextComponent(allMods ? "All the mods of target %1$s they ever joined with: %2$s" : "The mods of target %1$s in their current session: %2$s", target.getName(), String.join(", ", modList));
-				success++;
-			}
-			else
-				message = new TranslationTextComponent("No mod list of target %s was found", target.getName());
-
-			if (ctx.getSource().getEntity() instanceof ServerPlayerEntity) {
-				ServerPlayerEntity player = (ServerPlayerEntity)ctx.getSource().getEntity();
-
-				player.sendMessage(message, player.getUUID());
-			}
-			else
-				ctx.getSource().sendSuccess(message, false);
+			modList.addAll(allMods ? ModListObserver.getAllSessionMods(target) : ModListObserver.getCurrentMods(target));
 		}
 
-		return success;
+		if (targets.size() == 1) {
+			GameProfile target = targets.iterator().next();
+
+			if (modList.isEmpty())
+				message = new TranslationTextComponent("No mod list of target %s was found", target.getName());
+			else
+				message = new TranslationTextComponent(allMods ? "All mods target %1$s joined the server with since the last server restart: %2$s" : "The mods target %1$s last joined the server with: %2$s", target.getName(), TextComponentUtils.formatList(modList));
+		}
+		else if (modList.isEmpty())
+			message = new TranslationTextComponent("No mod lists of the %s targets were found", targets.size());
+		else
+			message = new TranslationTextComponent(allMods ? "All mods the %1$s targets joined the server with since the last server restart: %2$s" : "The mods the %1$s targets last joined the server with: %2$s", targets.size(), TextComponentUtils.formatList(modList));
+
+		if (ctx.getSource().getEntity() instanceof ServerPlayerEntity)
+			ctx.getSource().getEntity().sendMessage(message, ctx.getSource().getEntity().getUUID());
+		else
+			ctx.getSource().sendSuccess(message, false);
+
+		return modList.size();
 	}
 }
