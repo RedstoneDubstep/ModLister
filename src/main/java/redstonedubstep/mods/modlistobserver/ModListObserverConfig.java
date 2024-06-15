58c057ca3036a8575cb43fa71069332ff269f331
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2024-06-15 15:00:52 +0200

    Added config allowlist for mods that shouldn't get logged
    also updated build to work with java 21

--- src/main/java/redstonedubstep/mods/modlistobserver/ModListObserverConfig.java
+++ src/main/java/redstonedubstep/mods/modlistobserver/ModListObserverConfig.java
@@ -1,5 +1,8 @@
 package redstonedubstep.mods.modlistobserver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.tuple.Pair;
 
 import net.minecraftforge.common.ForgeConfigSpec;
@@ -20,7 +23,9 @@ public class ModListObserverConfig {
 	public static class Config {
 		public BooleanValue logJoiningModList;
 		public BooleanValue logServerMods;
+		public BooleanValue allowlistEnabled;
 		public IntValue modlistCommandPermissionLevel;
+		public ForgeConfigSpec.ConfigValue<List<String>> allowlist;
 
 		Config(ForgeConfigSpec.Builder builder) {
 			logJoiningModList = builder
@@ -29,9 +34,15 @@ public class ModListObserverConfig {
 			logServerMods = builder
 					.comment("Should mods of a joining player that are also present on the server be logged?")
 					.define("logServerMods", true);
+			allowlistEnabled = builder
+					.comment("Should only mods that are not allowlisted (via the \"allowlist\" config) be recorded and logged?")
+					.define("allowlistEnabled", false);
 			modlistCommandPermissionLevel = builder
 					.comment("What op permission level should be the requirement for being able to execute /modlist?")
 					.defineInRange("modlistCommandPermissionLevel", 3, 0, 4);
+			allowlist = builder
+					.comment("A comma-separated list of mod ids that are allowed by the server and thus shouldn't be recorded and logged")
+					.define("allowlist", new ArrayList<>());
 		}
 	}
 }
